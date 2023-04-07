c6f1e027143f2517a1e4941dc8db833e84f8123e
shivamanibrt <shivamanibrt03350@gmail.com>
2023-04-07 14:21:22 +1000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -3,18 +3,31 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
 import { NavigationBar } from './Components/NavigationBar';
 import { Tabledisplay } from './Components/Tabledisplay';
+import { useState } from 'react';
 
 function App() {
+  const [movieList, setMovieList] = useState([]);
 
+  const addMovieToList = movie => {
+    const tempArg = movieList.filter(i => i.imdbID !== movie.imdbID)
+    setMovieList([...tempArg, movie]);
+  }
+
+  const handelOnDelete = (imdbID) => {
+    if (window.confirm('Are you sure you want to delete')) {
+      const tempArg = movieList.filter(item => item.imdbId !== imdbID);
+      setMovieList(tempArg)
+    }
+  }
   return (
     <div className="App bg-dark text-dark">
       <Container>
 
         {/* This is navigation which consist display also*/}
-        <NavigationBar />
+        <NavigationBar addMovieToList={addMovieToList} />
 
         {/* Table display */}
-        <Tabledisplay />
+        <Tabledisplay movieList={movieList} handelOnDelete={handelOnDelete} />
       </Container>
     </div>
   );
--- src/Components/Tabledisplay.js
+++ src/Components/Tabledisplay.js
@@ -1,24 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button, Row, Col } from 'react-bootstrap'
 import { ButtonGroup } from 'react-bootstrap'
 import { CustomCard } from './CustomCard'
 
 
-export const Tabledisplay = () => {
+export const Tabledisplay = ({ movieList, handelOnDelete }) => {
+    const [displayList, setDisplayList] = useState([]);
+
+    useEffect(() => {
+        setDisplayList(movieList)
+    }, [movieList]);
+
+    const handelOnFilter = str => {
+        let tempArg = str === 'All'
+            ? movieList
+            : movieList.filter(item => item.mode === str);
+        setDisplayList(tempArg);
+    }
     return (
         <div>
             <ButtonGroup aria-label="Basic example">
-                <Button variant="primary">All</Button>
-                <Button variant="success">Nepali</Button>
-                <Button variant="info">Hindi</Button>
-                <Button variant="danger">English</Button>
+                <Button variant="primary" onClick={() => { handelOnFilter('All') }}>All</Button>
+                <Button variant="success" onClick={() => { handelOnFilter('Nepali') }}
+                >Nepali</Button>
+                <Button variant="info" onClick={() => { handelOnFilter('Hindi') }}>Hindi</Button>
+                <Button variant="danger" onClick={() => { handelOnFilter('English') }}>English</Button>
             </ButtonGroup>
-            <div className='py-3 text-warning'>0 Movies found</div>
+            <div className='py-3 text-warning'>{displayList.length} Movies found</div>
             <Row>
-                <CustomCard />
+                <Col className='d-flex gap-2 flex-wrap justify-content-around'>
+                    {
+                        displayList.map((item) => <CustomCard searchedMovies={item} func={handelOnDelete} />)
+                    }
+                </Col>
+
             </Row>
         </div>
-
-
     )
 }
